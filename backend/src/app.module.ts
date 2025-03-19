8f07dbc98e1c8db1332958e6a0776ab0258590f9
BangpleeBoi <peerapon.klajing@gmail.com>
2025-03-19 16:10:08 +0700

    wip(backend, tasks): add scaffolding for tasks CRUD

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigDebugService } from 'src/config/config.service';
 import { ConfigModule } from 'src/config/config.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [AuthModule, ConfigModule],
+  imports: [AuthModule, ConfigModule, TasksModule],
   providers: [AppService],
   controllers: [AppController],
 })
