9c2f98fe972b6371ba7159352f7ca97c0c61890a
BangpleeBoi <peerapon.klajing@gmail.com>
2025-04-02 10:19:14 +0700

    feat(backend, config): add frontend-backand url as env

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -1,29 +1,33 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
-  jwt: {
-    access: process.env.JWT_ACCESS_SECRET,
-    refresh: process.env.JWT_REFRESH_SECRET,
-    accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
-    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
-  },
-  database: {
-    url: process.env.DATABASE_URL,
-  },
-  aws: {
-    region: process.env.AWS_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    bucket: process.env.AWS_BUCKET_NAME,
-  },
-  smtp: {
-    host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT, 10),
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
-  },
-  google: {
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    redirectUri: process.env.GOOGLE_REDIRECT_URI,
-  },
-})
+    port: parseInt(process.env.PORT, 10) || 3000,
+    jwt: {
+        access: process.env.JWT_ACCESS_SECRET,
+        refresh: process.env.JWT_REFRESH_SECRET,
+        accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+        refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+    },
+    database: {
+        url: process.env.DATABASE_URL,
+    },
+    aws: {
+        region: process.env.AWS_REGION,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        bucket: process.env.AWS_BUCKET_NAME,
+    },
+    smtp: {
+        host: process.env.SMTP_HOST,
+        port: parseInt(process.env.SMTP_PORT, 10),
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+    },
+    google: {
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        redirectUri: process.env.GOOGLE_REDIRECT_URI,
+    },
+    url: {
+        client: process.env.CLIENT_URL,
+        backend: process.env.BACKEND_URL,
+    },
+});
